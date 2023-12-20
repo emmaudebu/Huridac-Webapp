1e6b46e5399eb3830218dfbdbeb2ff506e3b381e
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-12-20 11:44:54 +0100

    splash page

---
+++ huridac/src/Components/Splash.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Splash = () => {
+  return (
+    <main className="  w-screen h-[100vh] bg-[url('./images/Rectangle.png')] bg-cover bg-no-repeat flex items-center justify-end flex-col">
+      <a href="https://huridac.org/">
+        <button className=" bg-black  text-white w-[117px] h-[50px] mb-10 rounded-full">
+          GO
+        </button>
+      </a>
+    </main>
+  );
+};
+
+export default Splash;
