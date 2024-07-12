bba7530784677983cb50e1e2adb5f4dc0c9749a5
ifeanyi chukwu <emmanueludebu@gmail.com>
2024-07-13 00:56:23 +0100

    done

--- huridac/src/App.jsx
+++ huridac/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
+
 import Options from "./Components/Options";
 import Splash from "./Components/Splash";
 
 function App() {
   return (
     <>
-      {/* <Splash /> */}
-      <Options />
+      <Splash />
     </>
   );
 }
--- huridac/src/Components/Splash.jsx
+++ huridac/src/Components/Splash.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import Options from "./Options";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const Splash = () => {
   return (
     <main className="  w-screen h-[100vh] bg-[url('./images/Rectangle.png')] bg-cover bg-no-repeat flex items-center justify-end flex-col">
       <a href="https://huridac.org/">
-        <button className=" bg-black  text-white w-[127px] h-[50px] mb-10 rounded-full">
-          GO{" "}
-        </button>
+        <Link to="/Options">
+          <button className=" bg-black  text-white w-[127px] h-[50px] mb-10 rounded-full">
+            GO{" "}
+          </button>
+        </Link>
       </a>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/Options" element={<Options />} />
+      </Routes>
     </main>
   );
 };
